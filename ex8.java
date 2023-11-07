b6f5a75bae5679aecb4d184319f8033fab579d0d
yeongsu03 <150108475+yeongsu03@users.noreply.github.com>
2023-11-07 16:16:32 +0900

    Add files via upload

---
+++ ex8.java
@@ -0,0 +1,14 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex8 {
+		
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan. nextLine();			
+		System.out.print(str);
+
+	}
+
+}
