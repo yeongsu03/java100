3c4f63d6abb462ba136a696fcbc7a4aac73f62c7
yeongsu03 <150108475+yeongsu03@users.noreply.github.com>
2023-11-07 17:41:17 +0900

    Add files via upload

---
+++ ex13.java
@@ -0,0 +1,16 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex13 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		float num = scan.nextFloat();
+		
+		System.out.printf("%.2", num);
+		
+		scan.close();
+	}
+
+}
