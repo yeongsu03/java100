63314a0e267fb9045e34ae242735bd837ba7ccb2
yeongsu03 <150108475+yeongsu03@users.noreply.github.com>
2023-11-07 16:23:06 +0900

    Add files via upload

---
+++ ex9.java
@@ -0,0 +1,14 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex9 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Float r = scan.nextFloat();
+		System.out.print(r);
+
+	}
+
+}
