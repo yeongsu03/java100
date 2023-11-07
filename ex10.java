71cccef59827f7a09b6421dd9e66762f2f88d40e
yeongsu03 <150108475+yeongsu03@users.noreply.github.com>
2023-11-07 16:29:44 +0900

    Add files via upload

---
+++ ex10.java
@@ -0,0 +1,16 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex10 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.print(a+" "+b);
+		
+
+	}
+
+}
