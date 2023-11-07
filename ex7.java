7657c2a9cc2387b617164f30cc2fbd0d663f8f1d
yeongsu03 <150108475+yeongsu03@users.noreply.github.com>
2023-11-07 16:13:32 +0900

    Add files via upload

---
+++ ex7.java
@@ -0,0 +1,14 @@
+package java_ex100;
+
+import java.util.Scanner;
+
+public class ex7 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int i = scan. nextInt();
+		System.out.print(i);
+
+	}
+
+}
